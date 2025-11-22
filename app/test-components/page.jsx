102527b75e03c301ace6dcffa188ebb85e2c33c4
amad-khaly-diallo <amad-khaly.diallo@laplateforme.io>
2025-11-22 19:04:23 +0100

    .

--- app/test-components/page.jsx
+++ app/test-components/page.jsx
@@ -1,5 +1,5 @@
 import { ProductCard } from '../components/ProductCard/page';
-import styles from '../page.module.css';
+import styles from './page.module.css';
 
 export default function Cardcomponent() {
   // Exemples de produits pour la démonstration
